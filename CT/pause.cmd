db473278aad9ada8212d166a0a776b27b3dad455
miermontoto <underscoreBis@gmail.com>
2021-04-26 00:54:45 +0200

    Prácticas de 5.1 a 5.3

--- CT/pause.cmd
+++ CT/pause.cmd
@@ -1,1 +1,4 @@
 @Ensambla.exe %1 & @pause
+:: Programa que, en una línea, ejecuta el código .ens con el ensamblador y devuelve cualquier
+:: output que tenga el ejecutable, como información sobre errores de compilación. Se le debe
+:: pasar el archivo a ensamblar como parámetro (drag-n-drop encima de este archivo)
